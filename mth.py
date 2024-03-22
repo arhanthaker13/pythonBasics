e1acd97116854d925b89ff81962d81d4b7664902
arhanthaker13 <arhanthaker@gmail.com>
2024-03-22 04:10:01 -0400

    Math methods

---
+++ mth.py
@@ -0,0 +1,19 @@
+import math 
+
+pi = 3.14
+neg = -10
+x = 2
+y = 100
+z = 9
+
+print(pi)
+print(round(pi))
+print(abs(neg))
+print(pow(x, 3))
+print(max(x, y, z))
+print(min(x, y, z))
+print()
+
+print(math.ceil(pi))
+print(math.floor(pi))
+print(math.sqrt(420))
