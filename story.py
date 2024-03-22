727610b3aa445f61a313dea0228caf25ce4fa8a4
arhanthaker13 <arhanthaker@gmail.com>
2024-03-21 23:58:41 -0400

    Add if else statement

--- story.py
+++ story.py
@@ -1,12 +1,18 @@
 character_name = "John Cena"
 character_age = "18"
+is_male = True;
 
 char_age = 18
 
 print("My name is " + character_name + " AND")
 print("I am " + character_age + " years old.\n")
 
+character_name = "Mike"
+
 print(f"My name is {character_name} and I'm {char_age} years old.")
 print()
 
-print("NEW LINE")
+if is_male: 
+    print("MALE!")
+else: 
+    print("NOT MALE!")
