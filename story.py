9898a3a7ff85beff4aa553bffa46a9d9a1eac429
arhanthaker13 <arhanthaker@gmail.com>
2024-03-21 23:49:06 -0400

    Printing methods

---
+++ story.py
@@ -0,0 +1,12 @@
+character_name = "John Cena"
+character_age = "18"
+
+char_age = 18
+
+print("My name is " + character_name + " AND")
+print("I am " + character_age + " years old.\n")
+
+print(f"My name is {character_name} and I'm {char_age} years old.")
+print()
+
+print("NEW LINE")
