a98342c89338698a70011a4aa5f2f8100efde506
arhanthaker13 <arhanthaker@gmail.com>
2024-04-06 05:37:19 -0400

    Nested loops

--- nest.py
+++ nest.py
@@ -1,3 +1,23 @@
+rows = int(input("How many rows: "))
+coloumns = int(input("How many coloumns: "))
+symbol = input("Enter symbol you weant to print: ")
+
+for i in range(rows):
+    
+    for j in range(coloumns):
+        print(symbol, end="")
+    print()
+        
+#loop control
+
+phone_number = "123-456-7890"
+
+for i in phone_number:
+    if i == "-":
+        continue
+    print(i, end="") 
+
+
 for i in range(1, 21):
     
     if i == 13:
