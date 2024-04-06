04fd03228e2c72e095a922d87c74c7aaa0ac51bd
arhanthaker13 <arhanthaker@gmail.com>
2024-04-06 05:47:06 -0400

    2D lists basic

---
+++ py2DLists.py
@@ -0,0 +1,11 @@
+# 2D Lists
+
+drinks = ["coffee", "tea", "water"]
+dinner = ["rice", "chicker", "steak"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert] 
+
+print(food)
+print(food[0])
+print(food[2][1])
